a5d77703f6e7f9a8dc7a8244b76f73b3c817b9d4
Jesus Ramirez <jesus.ramirez@GDLA-MC-180723B.local>
2018-08-11 20:46:02 -0500

    playground examples

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Variables-Contants-DataTypes
+
+In this project we are going to use a Playground project that is another kind of projects that we can create in xcode
+
+# What is the difference between Xcode project and Xcode playground?
+
+An Xcode project allows you to create real apps that you could eventually upload to the App Store. An Xcode Playground allows you to play with code and test it out. It isn't for mainstream developing.
+
+# How to creat a Xcode playground
+
+1. Create a new playground with “File -> New -> Playground…”
+
+or 
+
+2. Open Xcode and in the first screen, select "Get started with a playground"
---
+++ Variables, Contants and Data Types.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+// import UIKit
+
+// - Variables and Constants are essentially containers for data
+// -- With Variables, you can change the value that's contained
+// -- With Constants you can only assign a value once
+// -- Both have a data type associated with them
+
+// Example of variable
+// Use camel case convention  to name variables
+var myAge : Int = 32
+
+// Please check to the right. The value is printing and updating in real time ---->
+myAge = 33
+
+print(myAge)
+
+//Example of Constants
+let myName : String = "Omar"
+
+// If you try to update a Constant you will get an error. Uncomment the next line to check.
+// myName = "Jesús"
+
+// Also you can open a bug console to check prints or errors. At the bottom select the first icon
+
+let myAgeInTenYears = myAge + 10
+
+// Concatenate strings
+let myFullName = myName + " Ramírez"
+
+// Another way
+let myFullName2 = "\(myName) Ramírez"
+
+// Using another constant for LastName
+let myLastName : String = "Ramírez"
+
+let myFullName3 = "\(myName) \(myLastName)"
+
+let myDetails = "\(myName), \(myAge)"
+
+let integers : Int = 123
+
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 3.1416
+
+let double : Double = 3.14159265359
+
+
